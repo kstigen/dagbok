cb57e69b8cd75f104bb3df0d19256350d433c91a
Kenneth Stigen <kenneth@stigen.net>
2014-10-24 21:11:43 +0200

    Prover en ny route

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello():
    return "Hello Flaskworld!"
 
+@app.route("/users/<user>")
+def users(user):
+	return "Heisann %s" % user
+
 if __name__ == '__main__':
 	app.run()
